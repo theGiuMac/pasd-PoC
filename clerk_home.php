a70eb964bf1b572f4e98c63ce6e915fb3dc5dc90
TheGiuMac <giulio@semidecidable.giulio.com>
2021-12-21 21:35:57 +0100

    Added clerk search button

--- clerk_home.php
+++ clerk_home.php
@@ -3,3 +3,20 @@ session_start();
 if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
 ?>
 
+<!DOCTYPE html>
+<html>
+
+<div class="button-fancy-container">
+    <form action="redirect_to_search_page.php" method="post">
+        <button class="button-fancy" type="submit">Search products</button>
+    </form>
+</div>
+
+</html>
+
+<?php
+} else {
+    header("Location: index.php");
+    exit();
+}
+?>

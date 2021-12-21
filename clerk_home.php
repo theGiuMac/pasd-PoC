cc04905821b09f3a839b67ba378f898316ca9de3
TheGiuMac <giulio@semidecidable.giulio.com>
2021-12-21 23:37:28 +0100

    Fixed homepage of clerk

--- clerk_home.php
+++ clerk_home.php
@@ -6,12 +6,27 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
 <!DOCTYPE html>
 <html>
 
-<div class="button-fancy-container">
-    <form action="redirect_to_search_page.php" method="post">
-        <button class="button-fancy" type="submit">Search products</button>
-    </form>
-</div>
+<head>
+    <title>Clerk Homepage</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
+<body>
+    <div class="button-fancy-container">
+        <form action="redirect_to_search_page.php" method="post">
+            <button class="button-fancy" type="submit">Search products</button>
+        </form>
+    </div>
+
+
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+
+    <a href="logout.php">Logout</a>
+
+</body>
 </html>
 
 <?php
